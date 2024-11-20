2160954107f9faf50e80239d4b79c8f1e6d460e3
Lara Campos <0000110195436xsp@al.educacao.sp.gov.br>
2024-11-20 15:12:29 -0300

    adicionando arquivos

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Escola',
+    'Quem é o Henrique?',
+    'O vice-diretor da escola Moraes Salles'
+)
+
+criaCartao(
+    'Geografia',
+    'Quem é a professora de geografia?',
+    'Claudinha'
+)
+
+criaCartao(
+    'Escola',
+    'Quem é o líder da minha sala?',
+    'Mariah'
+)
+
+criaCartao(
+    'Aulas',
+    'Qual a melhor aula pra dormir?',
+    'As aulas da Maria Cecília'
+)
